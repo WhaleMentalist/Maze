bcc22ed4c5c17048b52417ef443ebbbbfd107be3
Spencer Daniel <spencerdaniel93@gmail.com>
2018-02-06 00:37:21 -0600

    Added observer pattern for more efficient drawing. Need to fix some drawing bugs on lines.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys, time, pygame
 
-from maze import Maze
-from maze_graphic import Maze_Graphic
+from maze import *
 
+def draw(canvas, graphical_maze):
+    canvas.fill((255, 255, 255))
+    graphical_maze.draw_maze()
 
-def setup():
-    pygame.init()
+pygame.init()
+canvas = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
 
-white = (255, 255, 255)
-screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+# Create maze and display original
+maze = Maze(20)
+maze_cell_generator = maze.generate_single_cell()
+graphical_maze = Graphical_Maze(canvas, maze)
+draw(canvas, graphical_maze)
+pygame.display.flip()
 
-# Create maze
-maze = Maze(50)
-maze_generator = maze.generate_cell() # Generator to gradually build maze
 running = True
 
 while running:
@@ -24,12 +27,8 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     # Drawing
-    screen.fill(white)
-    maze = maze_generator.next() # Generate next part of maze
-    maze_graphic = Maze_Graphic(screen, maze)
-    maze_graphic.draw_maze()
+    maze = maze_cell_generator.next() # Generate next part of maze
     time.sleep(0.05)
-    pygame.display.flip()
 
 # Quit
 pygame.quit()
--- maze.py
+++ maze.py
@@ -1,47 +1,77 @@
-import random, sys, time
+import random, pygame
 
-class Maze:
+class Observable:
 
-    def __init__(self, s = 5):
-        self.size = s
-        self.stack = []
-        self.visited = set([]) # Keep a set of visited for generation algorithm and for drawing
-        self.popped = set([]) # Keep a set of popped for drawing
+    def __init_(self):
+        self.observers = []
 
-        self.cells = [] # Holds maze represented as a 2D grid
+    def add_observer(self, observer):
+        if observer not in self.observers:
+            self.observers.append(observer)
+        else:
+            print('Failed to add: {}'.format(observer))
+
+    def remove_observer(self, observer):
+        try:
+            self.observers.remove(observer)
+        except ValueError:
+            print('Failed to remove: {}'.format(observer))
+
+    # Notifies observers, which cell was changed and whether it was backtracked
+    # or explored
+    def notify_cell_changed(self, cell, state):
+        [observer.on_cell_changed(cell, state) for observer in self.observers]
+
+class Observer:
+
+    # Handle cell change events from observable
+    def on_cell_changed(self, cell, state):
+        pass
+
+class Maze(Observable):
+
+    def __init__(self, size = 5):
+        self.size = size
+        self.observers = []
+
+        self.cells = [] # Hold representation of maze as 2D grid
         for x in range(0, self.size):
-            self.cells.append([]) # Append an empty list before initializing cells in row
+            self.cells.append([]) # Append empty row to add columns to
             for y in range(0, self.size):
-                self.cells[x].append(Cell(x,y)) # Create cell with all the walls intact
+                self.cells[x].append(Cell(x,y))
 
-    # Algorithm generates a maze using depth first search with randomization
-    # for better maze
-    def generate_cell(self):
-        current = self.cells[0][0] # Pick top left as current cell to start
-        currPopped = self.cells[0][0]
-        self.visited.add((0, 0)) # Implement set to track visited cells
-        self.stack.append(current) # Push current onto stack
+    # Generator for dynamic drawing of maze generation on canvas.
+    # Yield will maintain the previous state, much like a stack in
+    # recursion
+    def generate_single_cell(self):
+        visited = set() # Track visited cells, useful for checking quickly for unvisited neighbors
+        stack = [] # Stack to hold explored cells
 
-        # While the stack has cells to backtrack, continue constructing maze
-        while len(self.stack) > 0:
-            current = self.stack[len(self.stack) - 1]
-            neighbors = self.get_unvisited_neighbors(current.x, current.y) # Find unvisited neighbors
+        visited.add((0, 0))
+        stack.append(self.cells[0][0])
+        self.notify_cell_changed(self.cells[0][0], 1)
 
-            # If there are no neighbors, we need to backtrack to node w/ neighbors
+        while len(stack) > 0:
+            currentCell = stack[len(stack) - 1] # Peeking at top of stack
+            neighbors = self.get_unvisited_neighbors(currentCell.x, currentCell.y, visited)
+
             if len(neighbors) == 0:
-                currPopped = self.stack.pop()
-                self.popped.add((currPopped.x, currPopped.y))
+                poppedCell = stack.pop()
+                self.notify_cell_changed(poppedCell, 0) # Notify that cell backtracked
+                print('Cell at (%s, %s) was backtracked' % (poppedCell.x, poppedCell.y))
             else:
                 neighbor = neighbors[random.randint(0, len(neighbors) - 1)]
-                self.connect_cells(current, neighbor)
-                self.stack.append(neighbor)
-                self.visited.add((neighbor.x, neighbor.y))
-                current = neighbor
+                self.connect_cells(currentCell, neighbor)
+                stack.append(neighbor)
+                visited.add((neighbor.x, neighbor.y))
+                self.notify_cell_changed(neighbor, 1)
+                currentCell = neighbor
 
-            yield self # Generate cell one at a time
+            yield self
 
-
-    # Method will connect cells with shared wall
+    # Method will connect cells by their shared wall. It
+    # uses the difference between position in grid to determine
+    # orientation.
     def connect_cells(self, cell_one, cell_two):
         # Need to figure out how cells are orientated
         if cell_one.x - cell_two.x == -1:
@@ -59,42 +89,109 @@ class Maze:
         else:
             print('Cells are not adjacent to each other')
 
-    # Method retrieves the neighbors of a given cell in maze and
-    # returns it as a list of cells
-    def get_unvisited_neighbors(self, x, y):
+    # Method retrieves unvisited neighbors of node located at
+    # 'x' and 'y' position in 2D grid. It will ensure boundaries
+    # allow neighbor to be possible as well.
+    def get_unvisited_neighbors(self, x, y, visited):
         neighbors = []
-
-        # Check 'x' coordinates to boundry
         if x > 0 and x < self.size - 1:
-            if (x - 1, y) not in self.visited:
+            if (x - 1, y) not in visited:
                 neighbors.append(self.cells[x - 1][y])
-            if (x + 1, y) not in self.visited:
+            if(x + 1, y) not in visited:
                 neighbors.append(self.cells[x + 1][y])
         elif x < self.size - 1:
-            if (x + 1, y) not in self.visited:
+            if (x + 1, y) not in visited:
                 neighbors.append(self.cells[x + 1][y])
         elif x > 0:
-            if (x - 1, y) not in self.visited:
+            if (x- 1, y) not in visited:
                 neighbors.append(self.cells[x - 1][y])
 
-        # Check 'y' coordinates to boundry
         if y > 0 and y < self.size - 1:
-            if (x, y - 1) not in self.visited:
+            if (x, y -1) not in visited:
                 neighbors.append(self.cells[x][y - 1])
-            if(x, y + 1) not in self.visited:
+            if (x, y + 1) not in visited:
                 neighbors.append(self.cells[x][y + 1])
         elif y < self.size - 1:
-            if (x, y + 1) not in self.visited:
+            if (x, y + 1) not in visited:
                 neighbors.append(self.cells[x][y + 1])
         elif y > 0:
-            if(x, y - 1) not in self.visited:
+            if (x, y - 1) not in visited:
                 neighbors.append(self.cells[x][y - 1])
-
         return neighbors
 
 class Cell:
-    # For walls the order is N, E, S, and W
+
+    # Initialize cell (i.e grid) in maze with all walls present
     def __init__(self, x, y):
-        self.walls = [True, True, True, True] # Represents wall around cell
+        self.walls = [True, True, True, True]
         self.x = x
         self.y = y
+
+# Object allows maze to be drawn on canvas
+class Graphical_Maze(Observer):
+
+    def __init__(self, canvas, maze):
+        self.canvas = canvas
+        self.maze = maze
+        maze.add_observer(self) # Add graphical representation as observer
+
+        width, height = self.canvas.get_size()
+        canvas_mid = (width / 2, height / 2)
+        paddedWidth = width - (2 * width * 0.1) # Add 10% padding on each side
+        paddedHeight = height - (2 * height * 0.1)
+
+        self.start = (width * 0.1, height * 0.1)
+        self.square_length = min(paddedWidth, paddedHeight) / self.maze.size
+        maze_size = self.square_length * self.maze.size
+        # Get middle point of maze, help adjust to middle of canvas
+        maze_mid = (self.start[0] + (maze_size / 2), self.start[1] + (maze_size / 2))
+        # Adjust maze to middle of screen
+        self.start = (self.start[0] + (canvas_mid[0] - maze_mid[0]), self.start[1] + (canvas_mid[1] - maze_mid[1]))
+
+    def draw_maze(self):
+        for y in range(0, self.maze.size):
+            for x in range(0, self.maze.size):
+                self.draw_cell(x, y)
+
+    def draw_cell(self, x, y, color = (255, 255, 255)):
+
+        # Fill in cell with color
+        top = (self.start[0] + self.square_length * x, self.start[1] + self.square_length * y)
+        size = self.square_length + 1
+        pygame.draw.rect(self.canvas, color, (top[0], top[1], size, size))
+
+        if self.maze.cells[x][y].walls[0] == True:
+            topLineStart = (self.start[0] + self.square_length * x, self.start[1] + self.square_length * y)
+            topLineEnd = (topLineStart[0] + self.square_length, topLineStart[1])
+            pygame.draw.line(self.canvas, (0, 0, 0), topLineStart, topLineEnd, 1)
+
+        if self.maze.cells[x][y].walls[2] == True:
+            bottomLineStart = (self.start[0] + self.square_length * x, self.start[1] + self.square_length * (y + 1))
+            bottomLineEnd = (bottomLineStart[0] + self.square_length, bottomLineStart[1])
+            pygame.draw.line(self.canvas, (0, 0, 0), bottomLineStart, bottomLineEnd, 1)
+
+        if self.maze.cells[x][y].walls[3] == True:
+            leftLineStart = (self.start[0] + self.square_length * x, self.start[1] + self.square_length * y)
+            leftLineEnd = (leftLineStart[0], leftLineStart[1] + self.square_length)
+            pygame.draw.line(self.canvas, (0, 0, 0), leftLineStart, leftLineEnd, 1)
+
+        if self.maze.cells[x][y].walls[1] == True:
+            rightLineStart = (self.start[0] + self.square_length * (x + 1), self.start[1] + self.square_length * y)
+            rightLineEnd = (rightLineStart[0], rightLineStart[1] + self.square_length)
+            pygame.draw.line(self.canvas, (0, 0, 0), rightLineStart, rightLineEnd, 1)
+
+        pygame.display.update((top[0], top[1], size, size)) # Update only portion of screen
+
+    # Method responds to cell change events from observable and
+    # fills cell with color based on state.
+    #
+    # STATE(S):
+    # 0 -> Cell was backtracked
+    # 1 -> Cell was explored
+    def on_cell_changed(self, cell, state):
+        if state == 0:
+            self.draw_cell(cell.x, cell.y, (150, 225, 0))
+        elif state == 1:
+            self.draw_cell(cell.x, cell.y, (150, 225, 225))
+        else:
+            self.draw_cell(cell.x, cell.y, (255, 255, 255))
